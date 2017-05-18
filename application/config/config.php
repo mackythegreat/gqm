463ed529bd190c2be0a2f927dc187eb1377165f9
Macky Sagales <mackysagales@gmail.com>
2017-05-18 15:54:55 +0800

    added htaccess and updated config file

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/gaea/';
+$config['base_url'] = 'http://localhost:8080/gqm/';
 
 /*
 |--------------------------------------------------------------------------
